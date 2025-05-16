13eccb928b5dbadb3b6225fbe7fbb37d34f00b0b
mahfouz72 <mohamadmahfouz39@gmail.com>
2025-05-16 22:42:39 +0300

    Setup for new parser

---
+++ src/main/java/com/puppycrawl/tools/checkstyle/JavadocCommentsAstVisitor.java
@@ -0,0 +1,7 @@
+package com.puppycrawl.tools.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.checks.javadoc.JavadocNodeImpl;
+import com.puppycrawl.tools.checkstyle.grammar.javadoc.JavadocCommentsParserBaseVisitor;
+
+public class JavadocCommentsAstVisitor extends JavadocCommentsParserBaseVisitor<JavadocNodeImpl> {
+}
