45e0e573bd5ace7ada58f4f6773e0562a194d95a
mahfouz72 <mohamadmahfouz39@gmail.com>
2025-05-16 22:42:39 +0300

    Setup for new parser

---
+++ src/test/java/com/puppycrawl/tools/checkstyle/grammar/javadoc/JavadocCommentsAstRegressionTest.java
@@ -0,0 +1,10 @@
+package com.puppycrawl.tools.checkstyle.grammar.javadoc;
+
+import com.puppycrawl.tools.checkstyle.AbstractTreeTestSupport;
+
+public class JavadocCommentsAstRegressionTest extends AbstractTreeTestSupport {
+    @Override
+    protected String getPackageLocation() {
+        return "com/puppycrawl/tools/checkstyle/grammar/javadoc/";
+    }
+}
