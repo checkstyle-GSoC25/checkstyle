9a37d58620c976cdb102094e817ec4b5fcc449f3
mahfouz72 <mohamadmahfouz39@gmail.com>
2025-05-19 16:15:07 +0300

    Issue #8: parse empty javadoc

--- src/main/java/com/puppycrawl/tools/checkstyle/JavadocCommentsAstVisitor.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/JavadocCommentsAstVisitor.java
@@ -1,7 +1,65 @@
 package com.puppycrawl.tools.checkstyle;
 
+import com.puppycrawl.tools.checkstyle.api.JavadocCommentsTokenTypes;
 import com.puppycrawl.tools.checkstyle.checks.javadoc.JavadocNodeImpl;
+import com.puppycrawl.tools.checkstyle.grammar.javadoc.JavadocCommentsParser;
 import com.puppycrawl.tools.checkstyle.grammar.javadoc.JavadocCommentsParserBaseVisitor;
+import com.puppycrawl.tools.checkstyle.utils.JavadocUtil;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import java.util.List;
 
 public class JavadocCommentsAstVisitor extends JavadocCommentsParserBaseVisitor<JavadocNodeImpl> {
+
+    @Override
+    public JavadocNodeImpl visitJavadoc(JavadocCommentsParser.JavadocContext ctx) {
+        final JavadocNodeImpl javadocNode = createImaginary(JavadocCommentsTokenTypes.JAVADOC);
+        processChildren(javadocNode, ctx.children.subList(0, ctx.children.size() - 1));
+        return javadocNode;
+    }
+
+    @Override
+    public JavadocNodeImpl visitMainDescription(JavadocCommentsParser.MainDescriptionContext ctx) {
+        return flattenedTree(ctx);
+    }
+
+    @Override
+    public JavadocNodeImpl visitJavadocLine(JavadocCommentsParser.JavadocLineContext ctx) {
+        return flattenedTree(ctx);
+    }
+
+    private JavadocNodeImpl flattenedTree(ParserRuleContext ctx) {
+        final JavadocNodeImpl dummyNode = new JavadocNodeImpl();
+        processChildren(dummyNode, ctx.children);
+        return (JavadocNodeImpl) JavadocUtil.getFirstChild(dummyNode);
+    }
+
+    private void processChildren(JavadocNodeImpl parent, List<? extends ParseTree> children) {
+        children.forEach(child -> {
+            if (child instanceof TerminalNode) {
+                TerminalNode terminalNode = (TerminalNode) child;
+                Token token = (Token) terminalNode.getPayload();
+                parent.addChild(create(token));
+            }
+            else {
+                parent.addChild(visit(child));
+            }
+        });
+    }
+
+    private JavadocNodeImpl create(Token token) {
+        final JavadocNodeImpl javadocNode = new JavadocNodeImpl();
+        javadocNode.initialize(token);
+        return javadocNode;
+    }
+
+    private JavadocNodeImpl createImaginary(int tokenType) {
+        final JavadocNodeImpl node = new JavadocNodeImpl();
+        node.setType(tokenType);
+        node.setText(JavadocUtil.getJavadocTokenName(tokenType));
+        return node;
+    }
 }
--- src/test/java/com/puppycrawl/tools/checkstyle/grammar/javadoc/JavadocCommentsAstRegressionTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/grammar/javadoc/JavadocCommentsAstRegressionTest.java
@@ -1,10 +1,29 @@
 package com.puppycrawl.tools.checkstyle.grammar.javadoc;
 
 import com.puppycrawl.tools.checkstyle.AbstractTreeTestSupport;
+import org.junit.jupiter.api.Test;
 
 public class JavadocCommentsAstRegressionTest extends AbstractTreeTestSupport {
     @Override
     protected String getPackageLocation() {
         return "com/puppycrawl/tools/checkstyle/grammar/javadoc/";
+    }
+
+    @Test
+    public void testEmptyJavadoc() throws Exception {
+        verifyJavadocTree(getPath("ExpectedEmptyJavadoc.txt"),
+                getPath("InputEmptyJavadoc.javadoc"));
+    }
+
+    @Test
+    public void testEmptyJavadocWithTabs() throws Exception {
+        verifyJavadocTree(getPath("ExpectedEmptyJavadocWithTabs.txt"),
+                getPath("InputEmptyJavadocWithTabs.javadoc"));
+    }
+
+    @Test
+    public void testEmptyJavadocStartsWithNewline() throws Exception {
+        verifyJavadocTree(getPath("ExpectedEmptyJavadocStartsWithNewline.txt"),
+                getPath("InputEmptyJavadocStartsWithNewline.javadoc"));
     }
 }
