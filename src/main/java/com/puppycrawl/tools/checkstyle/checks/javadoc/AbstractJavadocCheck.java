4dd16c3cbd2f9beeb20a9755b1a78652d043c8c8
Mohamed Mahfouz <mohamadmahfouz39@gmail.com>
2025-08-19 14:45:55 +0300

    Update AbstractJavadocTest and Connet Unclosed html message to the new parser (#67)

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/AbstractJavadocCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/AbstractJavadocCheck.java
@@ -317,7 +317,7 @@ public abstract class AbstractJavadocCheck extends AbstractCheck {
                 }
 
                 if (violateExecutionOnNonTightHtml && result.isNonTight()) {
-                    log(result.getFirstNonTightHtmlTag().getLine(),
+                    log(result.getFirstNonTightHtmlTag().getLineNumber(),
                             MSG_KEY_UNCLOSED_HTML_TAG,
                             result.getFirstNonTightHtmlTag().getText());
                 }
